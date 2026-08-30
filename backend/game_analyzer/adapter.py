036c1c23cd8f03e088757dc034365a2425fcb82c
Joseph Earl Weiner <josephearlweiner83@gmail.com>
2026-08-30 03:21:27 -0500

    Fix email confirmation URL to use pgnlens.com instead of localhost:5173

--- backend/game_analyzer/adapter.py
+++ backend/game_analyzer/adapter.py
@@ -1,6 +1,9 @@
 from allauth.account.adapter import DefaultAccountAdapter
+from django.conf import settings
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
-        return f'http://localhost:5173/confirm-email/{emailconfirmation.key}/'
+        protocol = 'https' if request and request.is_secure() else 'https'
+        domain = 'pgnlens.com'
+        return f'{protocol}://{domain}/confirm-email/{emailconfirmation.key}/'
